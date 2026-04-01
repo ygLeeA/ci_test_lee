1aa7d9bcc903a888a074df905a5098497f6e51ce
이영기B <108773775+YoungGiLee93@users.noreply.github.com>
2026-04-01 17:12:56 +0900

    Update App.js

--- src/main/view/src/App.js
+++ src/main/view/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>testsdssssdww</div>
+        <div>test</div>
       </header>
     </div>
   );
